b5d77f15202dbc2a9fd373f17439727d084de78e
Evans Abonyo <125830560+Evan5403@users.noreply.github.com>
2025-06-02 15:56:50 +0300

    Edit intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introTxt">I'm <span className="introName">Evans</span> <br/> Fullstack Developer</span> 
-                <p className="introPar">Am a fourth year student undetaking Bachelor in Information Technology at KCA University</p>
+                <p className="introPar">I'm a fourth year student undetaking Bachelor in Information Technology at KCA University</p>
                 {/* <Link><button className="btn"><BsBag /> <b>Hire Me</b></button></Link> */}
             </div>
             <img src={bg} alt="Profile" className="bgImg"/>
